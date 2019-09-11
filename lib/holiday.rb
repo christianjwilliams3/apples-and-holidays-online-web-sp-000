14147e49d4dc1090f59c541425081159ea5d3778
christian williams <christian.williamsj@gmail.com>
2019-09-11 00:09:14 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -23,6 +23,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
+  holiday_hash.each do |holiday_hash, supply|
   if season == :winter
 end
 

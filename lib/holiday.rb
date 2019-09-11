ec0f6a4576576f14395a15d7ff0862d5e84b5064
christian williams <christian.williamsj@gmail.com>
2019-09-11 00:25:38 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -48,7 +48,8 @@ def all_winter_holiday_supplies(holiday_hash)
 end
 
 def all_supplies_in_holidays(holiday_hash)
- holiday_hash.each do|season, holiday_name|
+ holiday_hash.each do|season, holidays|
+ 
 
 end
 

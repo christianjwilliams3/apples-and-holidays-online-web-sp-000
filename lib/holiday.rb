8e423233bfb0ff2738dfbac3fe2c88dfcddd39d3
christian williams <christian.williamsj@gmail.com>
2019-09-11 00:08:27 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -23,8 +23,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-  holiday_hash[:winter][:christmas] << "supply"
-  holiday_hash
+  if :season == 
 end
 
 
